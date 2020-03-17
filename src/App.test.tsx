a379b0d4707fc7c8bac68bdad685215576d577a7
LeeEunhyung <misoeun0520@gmail.com>
2020-03-17 17:53:46 +0900

    test 수정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
-    const { getByText } = render(<App />)
-    const linkElement = getByText(/learn react/i)
-    expect(linkElement).toBeInTheDocument()
+test('renders elements', () => {
+    const { getAllByText } = render(<App />)
+    const element = getAllByText(/Lirem ipsum/i)
+    expect(element).not.toBeNull()
 })
