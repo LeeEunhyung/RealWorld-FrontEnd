ff5581faf93d6c1b080d1fa51748e3a80f801c93
LeeEunhyung <misoeun0520@gmail.com>
2020-03-31 14:19:16 +0900

    Feed, YourFeed api 구분

--- src/apis/ArticlesApis.tsx
+++ src/apis/ArticlesApis.tsx
@@ -1,22 +1,12 @@
 import axios from 'axios'
 
 export class ArticlesApis {
-    static getListArticles(_offset: number) {
+    static getFeeds(_offset: number) {
         return axios({
             url: `https://conduit.productionready.io/api/articles?offset=${_offset}&limit=6`,
             method: 'GET',
         })
     }
-    static getFeeds(_offset: number) {
-        return axios({
-            url: `https://conduit.productionready.io/api/articles/feed?offset=${_offset}&limit=6`,
-            method: 'GET',
-            headers: {
-                Authorization: `Token ${localStorage.getItem('token')}`,
-            },
-        })
-    }
-
     static getYourFeeds(_offset: number) {
         return axios({
             url: `https://conduit.productionready.io/api/articles/feed?offset=${_offset}&limit=6`,
